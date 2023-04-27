177c9dd08ca561367641263b550594850bb1efc5
Valeriu1 <74401589+Valeriu1@users.noreply.github.com>
2023-04-27 22:33:03 +0200

    bulletHoles and Flash

--- Noise/Assets/Scripts/GunShoot.cs
+++ Noise/Assets/Scripts/GunShoot.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class GunShoot : MonoBehaviour
 {
-  public float damage = 10f;
-  public float range = 100f;
-  public Camera mainCamera;
+      public float damage = 10f;
+      public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+
+    private float nextTimeToFire = 0f;
+
+    public Camera mainCamera;
+      public ParticleSystem muzzleFlash;
+    public GameObject bulletHole;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
 
     void Shoot()
     {
+        if (!muzzleFlash.isPlaying) muzzleFlash.Play(); 
         RaycastHit hit;
-        if(Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
 
@@ -29,6 +38,12 @@ public class GunShoot : MonoBehaviour
             {
                 target.TakeDamage(damage);
             }
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+            GameObject tmpBulletHole = Instantiate(bulletHole, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(tmpBulletHole, 3f);
         }
     }
 }
