6a2806fa92bd77de31be91fb4106b54a13cd9d64
Valeriu1 <74401589+Valeriu1@users.noreply.github.com>
2023-05-06 22:55:21 +0200

    Update SaveOptions.cs

--- Noise/Assets/Scripts/SaveOptions.cs
+++ Noise/Assets/Scripts/SaveOptions.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
+using System.Threading.Tasks;
 
 public class SaveOptions : MonoBehaviour
 {
-    public float musicVolume;
-    public bool playMusic;
+     float musicVolume;
+     bool playMusic;
+    public Slider musicSlider;
+    public Toggle playMusicToggle;
 
     public AudioMixer audioMixer;
-
+    public AudioSource audioSource;
     private void Start()
     {
-
         LoadSettings();
     }
     public void LoadSettings()
     {
-        musicVolume = PlayerPrefs.GetFloat("Volume");
-        playMusic = PlayerPrefs.GetInt("PlayMusic") == 1;
+        if (PlayerPrefs.HasKey("Volume"))
+        {
+            musicVolume = PlayerPrefs.GetFloat("Volume");
+            musicSlider.value = musicVolume;
+        }
+        if (PlayerPrefs.HasKey("PlayMusic"))
+        {
+            playMusic = PlayerPrefs.GetInt("PlayMusic") == 1;
+            playMusicToggle.isOn = playMusic;
+        }
+    }
+
+    public void OnVolumeChanged()
+    {
+        musicVolume = musicSlider.value;
+        audioMixer.SetFloat("Volume", musicVolume);
+        PlayerPrefs.SetFloat("Volume", musicVolume);
+    }
+
+    public void OnMusicToggled()
+    {
+        playMusic = playMusicToggle.isOn;
+        audioSource.enabled = playMusic;
+        PlayerPrefs.SetInt("PlayMusic", playMusic ? 1 : 0);
+
     }
 
     public void SaveSettings()
