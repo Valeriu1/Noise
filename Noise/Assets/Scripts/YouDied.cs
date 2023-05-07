8a63fb48637aa6af0a43739ccd8a3fc540bbd6aa
Valeriu1 <74401589+Valeriu1@users.noreply.github.com>
2023-05-07 23:07:02 +0200

    Update YouDied.cs

--- Noise/Assets/Scripts/YouDied.cs
+++ Noise/Assets/Scripts/YouDied.cs
@@ -5,16 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class YouDied : MonoBehaviour
 {
-    public Canvas canvas;
-    public GameObject skelly;
-    public Canvas youDiedCanvas;
+    public GameObject canvas;
+    public GameObject youDiedCanvas;
+    public MouseLook mouseLook;
+    public PlayerMovement playerMovement;
 
 
     public void Dead()
     {
-        canvas.enabled = false;
-        youDiedCanvas.enabled = true;
-        skelly.SetActive(false);
+        canvas.SetActive(false);
+        youDiedCanvas.SetActive(true);
+        playerMovement.enabled = false;
+        mouseLook.enabled = false; 
+        Cursor.lockState = CursorLockMode.None;
     }
 
     public void Retry()
