67bbf7cdf2f354af5ea2dc561c1008100da130e9
Valeriu1 <74401589+Valeriu1@users.noreply.github.com>
2023-05-07 22:59:55 +0200

    update

--- Noise/Assets/Scripts/YouDied.cs
+++ Noise/Assets/Scripts/YouDied.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class YouDied : MonoBehaviour
 {
     public Canvas canvas;
+    public GameObject skelly;
     public Canvas youDiedCanvas;
 
 
@@ -13,6 +14,7 @@ public class YouDied : MonoBehaviour
     {
         canvas.enabled = false;
         youDiedCanvas.enabled = true;
+        skelly.SetActive(false);
     }
 
     public void Retry()
